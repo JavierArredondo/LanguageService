d03cc76acb7bff3882ddaaded0b1d72b3a1b5dc6
Javier <javier.arredondo.c@usach.cl>
2017-12-03 03:49:30 -0300

    Test case made for Python

---
+++ python/testCase.py
@@ -0,0 +1,5 @@
+import python/myCode.py
+a = suma(1,2)
+b = suma(2,3)
+c = suma(3,4)
+print (a, b, c)
---
+++ python/testCode.py
@@ -0,0 +1,9 @@
+def suma(x, y):
+	return x + y
+print ("Hola")
+a = suma(1,2)
+b = suma(2,3)
+c = suma(3,4)
+print (a)
+print (b)
+print (c)
